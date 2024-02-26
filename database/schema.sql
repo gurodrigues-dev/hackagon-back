7a211117a45dcdd24e019181e5529ee423596a72
Ozzetti <gustavo.ozzetti@aluno.faculdadeimpacta.com.br>
2024-02-26 19:59:45 -0300

    atualizaçao da database

--- database/schema.sql
+++ database/schema.sql
@@ -1,25 +1,32 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+    nickname VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
+    point INTEGER
+);
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE questions (
+    question_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    date DATE,
+    level INTEGER,
+    params1 TEXT,
+    response1 TEXT,
+    params2 TEXT,
+    response2 TEXT,
+    params3 TEXT,
+    response3 TEXT
+    );
+
+CREATE TABLE answers (
+    answers_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    nickname VARCHAR(50) NOT NULL,
+    questionid UUID NOT NULL,
+    status VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE answers (
-    answer_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    question_id INT NOT NULL,
-    answer_text TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (question_id) REFERENCES questions(question_id)
-);
-
-CREATE TABLE questions (
-    question_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    question_text TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
