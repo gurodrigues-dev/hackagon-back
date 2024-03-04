275d8f2f06814f26f1d876166c0bd0498b5229e0
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-04 18:54:33 -0300

    feat: implementing aws

--- repository/aws.go
+++ repository/aws.go
@@ -3,10 +3,12 @@ package repository
 import (
 	"context"
 	"gin/config"
+	"gin/types"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
 )
 
 type AWS struct {
@@ -34,6 +36,53 @@ func NewAwsConnection() (*AWS, error) {
 
 }
 
-func (a *AWS) SendEmail(ctx context.Context, email *string) error {
+func (a *AWS) VerifyEmail(ctx context.Context, email *string) error {
+
+	svc := ses.New(a.conn)
+
+	emailVerified := &ses.VerifyEmailIdentityInput{
+		EmailAddress: aws.String(*email),
+	}
+
+	_, err := svc.VerifyEmailIdentity(emailVerified)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
+
+}
+
+func (a *AWS) SendEmail(ctx context.Context, email *types.Email) error {
+
+	conf := config.Get()
+
+	svc := ses.New(a.conn)
+
+	emailInput := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			ToAddresses: []*string{aws.String(email.Recipient)},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Text: &ses.Content{
+					Data: aws.String(email.Body),
+				},
+			},
+			Subject: &ses.Content{
+				Data: aws.String(email.Subject),
+			},
+		},
+		Source: aws.String(conf.Aws.Source),
+	}
+
+	_, err := svc.SendEmail(emailInput)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
