9f7b00059e7fe3278eb9dbf5a7298e968f54e415
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-05 01:09:27 -0300

    refactor: unused lines

--- internal/controllers/controllers.go
+++ internal/controllers/controllers.go
@@ -184,8 +184,6 @@ func (ct *controller) createUser(c *gin.Context) {
 
 	exists, _ := ct.service.VerifyEmailExists(c, &input.Email)
 
-	fmt.Println(exists, input.Email)
-
 	if exists {
 		log.Println("email already exists!")
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
@@ -366,6 +364,58 @@ func (ct *controller) GetRank(c *gin.Context) {
 
 }
 
+func (ct *controller) sendEmailToRecovery(c *gin.Context) {
+
+	var input types.User
+
+	if err := c.BindJSON(&input); err != nil {
+		log.Printf("error parsing body content: %s", err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid body content"})
+		return
+	}
+
+	exists, err := ct.service.VerifyEmailExists(c, &input.Email)
+
+	if !exists {
+		log.Printf("error while searching email: %s", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+	token, err := ct.service.GenerateRandomToken()
+
+	if err != nil {
+		log.Printf("error at creating token: %s", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+	email := types.Email{
+		Subject:   "Hackagon - Token to recovery your account",
+		Body:      fmt.Sprintf("Hey! How are you? Let me see... You forgot your password. Oh, it's a shame. Your token's here: %s", token),
+		Recipient: &input.Email,
+	}
+
+	err = ct.service.SendEmailToRecovery(c, &email)
+
+	if err != nil {
+		log.Printf("error at creating token: %s", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+	err = ct.service.SaveRedis(c, &token, &input.Email)
+
+	if err != nil {
+		log.Printf("error while saving token: %s", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "token send w/ successfully"})
+
+}
+
 func (ct *controller) Start() {
 
 	conf := config.Get()
@@ -419,6 +469,7 @@ func (ct *controller) Start() {
 	api.POST("/answer", authMiddleware, ct.CreateAnswer)
 	api.DELETE("/answer/:id", authMiddleware, ct.DeleteAnswer)
 	api.GET("/rank", authMiddleware, ct.GetRank)
+	api.POST("/password", ct.sendEmailToRecovery)
 
 	router.Run(fmt.Sprintf(":%d", conf.Server.Port))
 
