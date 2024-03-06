4981fcd95f422c02578c8460b7890c21908f87fb
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-06 17:36:44 -0300

    hotfix: adding patch in cors

--- internal/controllers/controllers.go
+++ internal/controllers/controllers.go
@@ -444,7 +444,7 @@ func (ct *controller) Start() {
 	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
